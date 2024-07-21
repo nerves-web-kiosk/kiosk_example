02bf2bbf255c258db66fe95cd580e5c79ed6b397
Ryota Kinukawa <kinukawa@tombo-works.com>
2024-07-21 09:57:54 +0900

    Enable os_mon

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule KioskExample.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger, :runtime_tools],
+      extra_applications: [:logger, :runtime_tools, :os_mon],
       mod: {KioskExample.Application, []}
     ]
   end
